2245679573f5113b2d626bc5ba8207abc3353921
Kafcis <47874210+Kafcis@users.noreply.github.com>
2022-03-11 14:27:54 +0800

    more poses for detection

--- data_col.py
+++ data_col.py
@@ -43,7 +43,7 @@ renderer = MovenetRenderer(
     pose,
     output=args.output)
 #edit the file below to make new poses
-file = open('tempcsv/rightdab.csv', 'w')
+file = open('tempcsv/rightsuperman.csv', 'w')
 file.truncate(0)
 writer = csv.writer(file)
 

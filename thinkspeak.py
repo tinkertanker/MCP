08ffd3585c1f3a9f74bb455da225e0cdb8471bca
Steven Chan <steven@tinkertanker.com>
2022-05-31 18:34:41 +0800

    add timeout for sending to thingspeak

--- thinkspeak.py
+++ thinkspeak.py
@@ -2,6 +2,7 @@
 
 import socket
 import os
+import time
 import urllib.request
 
 # Upload number of people to ThinkSpeak
@@ -23,20 +24,25 @@ if __name__ == '__main__':
 
     succeeded = False
     peoplenum = 0
+    starttime = time.time()
 
-    while not succeeded:
+    while time.time() - starttime < 10: # keep trying for 10 seconds
         if os.path.exists(args.peoplenum_filename):
             try:
                 with open(args.peoplenum_filename) as f:
                     peoplenum = int(f.readline())
                     print(f'read peoplenum of {peoplenum} from file')
                     succeeded = True
+                    break
             except:
                 pass
 
-    hostname = args.hostname or socket.gethostname()
-    with urllib.request.urlopen(f'https://api.thingspeak.com/update.json?api_key=5NO8KS8QNN4BR9WY&field{hostname[-1:]}={peoplenum}') as f:
-        print(f.read().decode('utf-8'))
+    if succeeded:
+        hostname = args.hostname or socket.gethostname()
+        with urllib.request.urlopen(f'https://api.thingspeak.com/update.json?api_key=5NO8KS8QNN4BR9WY&field{hostname[-1:]}={peoplenum}') as f:
+            print(f.read().decode('utf-8'))
+    else:
+        print("Timed out.")
 
     
     

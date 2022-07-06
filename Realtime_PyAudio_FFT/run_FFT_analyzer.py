be19f49575116058fdbe2751f7f36c5dde80f7ed
Marina Pi <marinapi@m.tinkertanker.com>
2022-07-06 12:28:47 +0800

    changes from office deploy

--- Realtime_PyAudio_FFT/run_FFT_analyzer.py
+++ Realtime_PyAudio_FFT/run_FFT_analyzer.py
@@ -68,7 +68,7 @@ def convert_window_ratio(window_ratio):
 def run_FFT_analyzer():
     args = parse_args()
 
-    lc = LightControl(simulate=True, alt_mapping=False)
+    lc = LightControl(simulate=False, alt_mapping=True)
 
     for x in range(0,9):
         for y in range(0,7):
@@ -97,7 +97,7 @@ def run_FFT_analyzer():
                     updates_per_second  = 1000,  # How often to read the audio stream for new data
                     smoothing_length_ms = 50,    # Apply some temporal smoothing to reduce noisy features
                     n_frequency_bins = args.frequency_bins, # The FFT features are grouped in bins
-                    visualize = 1,               # Visualize the FFT features with PyGame
+                    visualize = 0,               # Visualize the FFT features with PyGame
                     verbose   = args.verbose,    # Print running statistics (latency, fps, ...)
                     height    = args.height,     # Height, in pixels, of the visualizer window,
                     window_ratio = window_ratio  # Float ratio of the visualizer window. e.g. 24/9
@@ -105,20 +105,19 @@ def run_FFT_analyzer():
 
     fps = 15  #How often to update the FFT features + display
     last_update = time.time()
-    energy_threshold_low = 2
-    energy_threshold_high = 10
+    energy_threshold_low = 1.5
+    energy_threshold_high = 5
     while True:
         if (time.time() - last_update) > (1./fps):
             last_update = time.time()
             raw_fftx, raw_fft, binned_fftx, binned_fft = ear.get_audio_features()
             frequency_bin_energies = ear.frequency_bin_energies / ear.bin_mean_values
-            print(frequency_bin_energies)
             for i in range(0, len(frequency_bin_energies)):
                 energy = frequency_bin_energies[i]
                 energy = max(energy, energy_threshold_low)
                 energy = min(energy, energy_threshold_high)
-                hue = 20 * energy / 360
-                color = colorsys.hsv_to_rgb(hue, 0.8, energy/energy_threshold_high)
+                hue = (90 + 40 * energy) / 360
+                color = colorsys.hsv_to_rgb(hue, 0.8, (energy - 1.4)/energy_threshold_high)
                 r = int(color[0] * 255.0)
                 g = int(color[1] * 255.0)
                 b = int(color[2] * 255.0)
